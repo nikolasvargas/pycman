ab5c293d400c6de1b4eb50fdce156164c2f5c761
Níkolas Vargas <vargasnikolass@gmail.com>
2020-04-21 15:14:21 -0300

    clean code?

--- chars.py
+++ chars.py
@@ -8,18 +8,18 @@ def pacman(window: Surface, pos: Tuple[int]) -> None:
     radius: int = 50
     fill: int = 0
 
-    def _b() -> Rect:
+    def _lips() -> Rect:
         x_r: Tuple[int] = (x + radius, y - radius)
         y_r: Tuple[int] = (x + radius, y)
         polygon_rects: List[int] = [circle_rect, x_r, y_r]
         return draw.polygon(window, COLOR['DARK'], polygon_rects, fill)
 
-    def _e() -> Rect:
+    def _eyes() -> Rect:
         e_x = (x + (radius // 3))
         e_y = (y - int(radius * 0.5))
         e_radius = radius // 10
         return draw.circle(window, COLOR['DARK'], (e_x, e_y), e_radius, fill)
 
     draw.circle(window, COLOR['YELLOW'], circle_rect, radius, fill)
-    _b()
-    _e()
+    _lips()
+    _eyes()
